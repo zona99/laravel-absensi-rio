4a032585df4eb110d509b17f152d7045cdbea07d
zona99 <rio.triadi08@gmail.com>
2024-05-01 08:43:46 +0700

    fortify, login, logout

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,20 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('pages.auth.auth-login');
 });
 
 Route::get('/dashboard', function () {
     return view('pages.dashboard', ['type_menu' => 'dashboard']);
+});
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('home', function () {
+        return view('pages.dashboard', ['type_menu' => 'home']);
+    })->name('home');
+
+    Route::resource('users', UserController::class);
 });
